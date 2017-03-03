dffae81df1a87bab568f21a77e3be05bfa4bd4ca
Aram Koukia <aramkoukia@gmail.com>
2017-03-02 16:36:48 -0800

    added 100 sample nodes

--- assets/js/network-canvas.js
+++ assets/js/network-canvas.js
@@ -8,246 +8,106 @@ class NetworkSimulation {
                 y: 500,
                 isBase: true,
             },                
-            {
-                title: "Node 1",
-                x: 410,
-                y: 360,
-                isBase: false,
-            },
-            {
-                title: "Node 2",
-                x: 220,
-                y: 100,
-                isBase: false,
-            },
-            {
-                title: "Node 3",
-                x: 300,
-                y: 810,
-                isBase: false,
-            },
-            {
-                title: "Node 4",
-                x: 990,
-                y: 90,
-                isBase: false,
-            },
-            {
-                title: "Node 5",
-                x: 70,
-                y: 870,
-                isBase: false,
-            },
-            {
-                title: "Node 6",
-                x: 90,
-                y: 50,
-                isBase: false,
-            },
-            {
-                title: "Node 7",
-                x: 900,
-                y: 60,
-                isBase: false,
-            },
-            {
-                title: "Node 8",
-                x: 650,
-                y: 110,
-                isBase: false,
-            },
-            {
-                title: "Node 9",
-                x: 390,
-                y: 160,
-                isBase: false,
-            },
-            {
-                title: "Node 10",
-                x: 680,
-                y: 440,
-                isBase: false,
-            },
-            {
-                title: "Node 11",
-                x: 90,
-                y: 470,
-                isBase: false,
-            },
-            {
-                title: "Node 12",
-                x: 840,
-                y: 810,
-                isBase: false,
-            },
-            {
-                title: "Node 13",
-                x: 400,
-                y: 30,
-                isBase: false,
-            },
-            {
-                title: "Node 14",
-                x: 770,
-                y: 80,
-                isBase: false,
-            },
-            {
-                title: "Node 15",
-                x: 450,
-                y: 550,
-                isBase: false,
-            },
-            {
-                title: "Node 16",
-                x: 350,
-                y: 440,
-                isBase: false,
-            },
-            {
-                title: "Node 17",
-                x: 780,
-                y: 670,
-                isBase: false,
-            },
-            {
-                title: "Node 18",
-                x: 100,
-                y: 530,
-                isBase: false,
-            },
-            {
-                title: "Node 19",
-                x: 900,
-                y: 890,
-                isBase: false,
-            },
-            {
-                title: "Node 20",
-                x: 760,
-                y: 840,
-                isBase: false,
-            },
-            {
-                title: "Node 21",
-                x: 890,
-                y: 30,
-                isBase: false,
-            },
-            {
-                title: "Node 22",
-                x: 780,
-                y: 390,
-                isBase: false,
-            },
-            {
-                title: "Node 23",
-                x: 750,
-                y: 410,
-                isBase: false,
-            },
-            {
-                title: "Node 24",
-                x: 700,
-                y: 200,
-                isBase: false,
-            },
-            {
-                title: "Node 25",
-                x: 610,
-                y: 940,
-                isBase: false,
-            },
-            {
-                title: "Node 26",
-                x: 540,
-                y: 500,
-                isBase: false,
-            },
-            {
-                title: "Node 27",
-                x: 250,
-                y: 570,
-                isBase: false,
-            },
-            {
-                title: "Node 28",
-                x: 40,
-                y: 30,
-                isBase: false,
-            },
-            {
-                title: "Node 29",
-                x: 810,
-                y: 870,
-                isBase: false,
-            },
-            {
-                title: "Node 30",
-                x: 430,
-                y: 430,
-                isBase: false,
-            },
-            {
-                title: "Node 31",
-                x: 600,
-                y: 750,
-                isBase: false,
-            },
-            {
-                title: "Node 32",
-                x: 330,
-                y: 790,
-                isBase: false,
-            },
-            {
-                title: "Node 33",
-                x: 540,
-                y: 750,
-                isBase: false,
-            },
-            {
-                title: "Node 34",
-                x: 30,
-                y: 220,
-                isBase: false,
-            },
-            {
-                title: "Node 35",
-                x: 790,
-                y: 690,
-                isBase: false,
-            },
-            {
-                title: "Node 36",
-                x: 690,
-                y: 940,
-                isBase: false,
-            },
-            {
-                title: "Node 37",
-                x: 400,
-                y: 70,
-                isBase: false,
-            },
-            {
-                title: "Node 38",
-                x: 270,
-                y: 920,
-                isBase: false,
-            },
-            {
-                title: "Node 39",
-                x: 910,
-                y: 110,
-                isBase: false,
-            },
-            {
-                title: "Node 40",
-                x: 860,
-                y: 240,
-                isBase: false,
-            }                 
+{ title: "Node 1", x: 719, y: 368, isBase: false, },
+{ title: "Node 2", x: 736, y: 989, isBase: false, },
+{ title: "Node 3", x: 372, y: 557, isBase: false, },
+{ title: "Node 4", x: 327, y: 431, isBase: false, },
+{ title: "Node 5", x: 560, y: 589, isBase: false, },
+{ title: "Node 6", x: 392, y: 332, isBase: false, },
+{ title: "Node 7", x: 903, y: 282, isBase: false, },
+{ title: "Node 8", x: 732, y: 360, isBase: false, },
+{ title: "Node 9", x: 812, y: 370, isBase: false, },
+{ title: "Node 10", x: 643, y: 498, isBase: false, },
+{ title: "Node 11", x: 68, y: 401, isBase: false, },
+{ title: "Node 12", x: 786, y: 540, isBase: false, },
+{ title: "Node 13", x: 2, y: 642, isBase: false, },
+{ title: "Node 14", x: 881, y: 388, isBase: false, },
+{ title: "Node 15", x: 530, y: 323, isBase: false, },
+{ title: "Node 16", x: 955, y: 789, isBase: false, },
+{ title: "Node 17", x: 780, y: 961, isBase: false, },
+{ title: "Node 18", x: 331, y: 959, isBase: false, },
+{ title: "Node 19", x: 463, y: 450, isBase: false, },
+{ title: "Node 20", x: 73, y: 412, isBase: false, },
+{ title: "Node 21", x: 509, y: 475, isBase: false, },
+{ title: "Node 22", x: 857, y: 829, isBase: false, },
+{ title: "Node 23", x: 698, y: 191, isBase: false, },
+{ title: "Node 24", x: 318, y: 76, isBase: false, },
+{ title: "Node 25", x: 802, y: 441, isBase: false, },
+{ title: "Node 26", x: 780, y: 946, isBase: false, },
+{ title: "Node 27", x: 736, y: 603, isBase: false, },
+{ title: "Node 28", x: 834, y: 793, isBase: false, },
+{ title: "Node 29", x: 789, y: 450, isBase: false, },
+{ title: "Node 30", x: 115, y: 18, isBase: false, },
+{ title: "Node 31", x: 104, y: 684, isBase: false, },
+{ title: "Node 32", x: 603, y: 818, isBase: false, },
+{ title: "Node 33", x: 547, y: 819, isBase: false, },
+{ title: "Node 34", x: 497, y: 881, isBase: false, },
+{ title: "Node 35", x: 546, y: 924, isBase: false, },
+{ title: "Node 36", x: 333, y: 779, isBase: false, },
+{ title: "Node 37", x: 262, y: 725, isBase: false, },
+{ title: "Node 38", x: 15, y: 328, isBase: false, },
+{ title: "Node 39", x: 955, y: 562, isBase: false, },
+{ title: "Node 40", x: 743, y: 20, isBase: false, },
+{ title: "Node 41", x: 332, y: 8, isBase: false, },
+{ title: "Node 42", x: 709, y: 304, isBase: false, },
+{ title: "Node 43", x: 130, y: 369, isBase: false, },
+{ title: "Node 44", x: 251, y: 172, isBase: false, },
+{ title: "Node 45", x: 927, y: 557, isBase: false, },
+{ title: "Node 46", x: 207, y: 949, isBase: false, },
+{ title: "Node 47", x: 508, y: 199, isBase: false, },
+{ title: "Node 48", x: 13, y: 965, isBase: false, },
+{ title: "Node 49", x: 895, y: 370, isBase: false, },
+{ title: "Node 50", x: 34, y: 657, isBase: false, },
+{ title: "Node 51", x: 675, y: 754, isBase: false, },
+{ title: "Node 52", x: 949, y: 587, isBase: false, },
+{ title: "Node 53", x: 740, y: 599, isBase: false, },
+{ title: "Node 54", x: 760, y: 176, isBase: false, },
+{ title: "Node 55", x: 817, y: 951, isBase: false, },
+{ title: "Node 56", x: 142, y: 759, isBase: false, },
+{ title: "Node 57", x: 392, y: 750, isBase: false, },
+{ title: "Node 58", x: 983, y: 907, isBase: false, },
+{ title: "Node 59", x: 735, y: 678, isBase: false, },
+{ title: "Node 60", x: 212, y: 734, isBase: false, },
+{ title: "Node 61", x: 845, y: 578, isBase: false, },
+{ title: "Node 62", x: 588, y: 583, isBase: false, },
+{ title: "Node 63", x: 618, y: 970, isBase: false, },
+{ title: "Node 64", x: 982, y: 543, isBase: false, },
+{ title: "Node 65", x: 230, y: 358, isBase: false, },
+{ title: "Node 66", x: 553, y: 605, isBase: false, },
+{ title: "Node 67", x: 101, y: 865, isBase: false, },
+{ title: "Node 68", x: 737, y: 312, isBase: false, },
+{ title: "Node 69", x: 206, y: 53, isBase: false, },
+{ title: "Node 70", x: 206, y: 347, isBase: false, },
+{ title: "Node 71", x: 702, y: 932, isBase: false, },
+{ title: "Node 72", x: 260, y: 433, isBase: false, },
+{ title: "Node 73", x: 509, y: 304, isBase: false, },
+{ title: "Node 74", x: 623, y: 920, isBase: false, },
+{ title: "Node 75", x: 725, y: 45, isBase: false, },
+{ title: "Node 76", x: 870, y: 289, isBase: false, },
+{ title: "Node 77", x: 589, y: 266, isBase: false, },
+{ title: "Node 78", x: 436, y: 126, isBase: false, },
+{ title: "Node 79", x: 35, y: 662, isBase: false, },
+{ title: "Node 80", x: 376, y: 303, isBase: false, },
+{ title: "Node 81", x: 680, y: 186, isBase: false, },
+{ title: "Node 82", x: 729, y: 778, isBase: false, },
+{ title: "Node 83", x: 714, y: 287, isBase: false, },
+{ title: "Node 84", x: 636, y: 366, isBase: false, },
+{ title: "Node 85", x: 425, y: 455, isBase: false, },
+{ title: "Node 86", x: 268, y: 567, isBase: false, },
+{ title: "Node 87", x: 709, y: 774, isBase: false, },
+{ title: "Node 88", x: 959, y: 266, isBase: false, },
+{ title: "Node 89", x: 845, y: 819, isBase: false, },
+{ title: "Node 90", x: 593, y: 472, isBase: false, },
+{ title: "Node 91", x: 677, y: 840, isBase: false, },
+{ title: "Node 92", x: 822, y: 770, isBase: false, },
+{ title: "Node 93", x: 621, y: 1, isBase: false, },
+{ title: "Node 94", x: 708, y: 419, isBase: false, },
+{ title: "Node 95", x: 915, y: 315, isBase: false, },
+{ title: "Node 96", x: 572, y: 570, isBase: false, },
+{ title: "Node 97", x: 441, y: 481, isBase: false, },
+{ title: "Node 98", x: 55, y: 436, isBase: false, },
+{ title: "Node 99", x: 150, y: 417, isBase: false, },
+{ title: "Node 100", x: 216, y: 561, isBase: false, }
         ];
     }
 
@@ -301,14 +161,12 @@ class NetworkSimulation {
                     {                            
                         if(node.title != nodes[i].title && Math.sqrt(Math.pow(Math.abs(node.x - nodes[i].x),2) + Math.pow(Math.abs(node.y - nodes[i].y),2)) <= 50)
                         {
-                            // draw line
-                            // ctx.beginPath();
-                            // ctx.strokeStyle = "#888";
-                            // ctx.setLineDash([0, 0]); 
-                            // ctx.moveTo(node.x, node.y);
-                            // ctx.lineTo(nodes[i].x, nodes[i].y);
-                            // ctx.stroke();      
                             networkSimulation.AnimateDrawLine(ctx, 0, node.x, node.y, nodes[i].x, nodes[i].y);
+
+                            // Draw arrow heads
+                            // var endRadians = Math.atan((nodes[i].y - node.y) / (nodes[i].x - node.x));
+                            // endRadians += ((nodes[i].x > node.x) ? 90 : -90) * Math.PI/180;
+                            // networkSimulation.DrawArrowHead(ctx, nodes[i].x, nodes[i].y, endRadians);                            
 
                             toastr.success("Whisper sent to neighbor node: " + nodes[i].title);
                             neighborCount++;
@@ -466,6 +324,20 @@ class NetworkSimulation {
         // this, and this is part of an outline, 
         // we probably also want a ctx.closePath()
     }
+
+    // DrawArrowHead (ctx,x,y,radians) {
+    //     ctx.save();
+    //     ctx.beginPath();
+    //     ctx.translate(x,y);
+    //     ctx.rotate(radians);
+    //     ctx.moveTo(0,0);
+    //     ctx.lineTo(5,20);
+    //     ctx.lineTo(-5,20);
+    //     ctx.closePath();
+    //     ctx.restore();
+    //     ctx.fillStyle = "black";
+    //     ctx.fill();
+    // }    
 
 
 }
